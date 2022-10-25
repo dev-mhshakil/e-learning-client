0f9130fb1e15a61424aec837fd2e3d822ec0bc6d
sneoo <epicshakil@gmail.com>
2022-10-25 12:47:25 +0600

    changed course card design

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -7,13 +7,13 @@ const Courses = () => {
   const courses = useLoaderData();
   console.log(courses);
   return (
-    <div className="grid grid-cols-3 gap-4 mt-10">
+    <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-4 mt-10 ">
       <div>
         {courses.map((course) => (
           <LeftSide key={course.id} course={course}></LeftSide>
         ))}
       </div>
-      <div className="col-span-2 grid grid-cols-3 gap-4">
+      <div className="col-span-2 grid lg:grid-cols-3 sm:grid-cols-1 gap-4">
         {courses.map((course) => (
           <SingleCourse key={course.id} course={course}></SingleCourse>
         ))}
--- src/components/SingleCourse/SingleCourse.js
+++ src/components/SingleCourse/SingleCourse.js
@@ -1,18 +1,51 @@
 import React from "react";
+import { IoIosTimer } from "react-icons/io";
+import { HiOutlineBookOpen, HiStar } from "react-icons/hi";
 
 const SingleCourse = ({ course }) => {
-  const { title, details, imgURL } = course;
+  const { id, title, imgURL, author, fees, rating, courseTime, modules } =
+    course;
+
+  const handleDetails = () => {};
+
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img src={imgURL} alt="Shoes" className="rounded-xl" />
+          <img src={imgURL} alt="Shoes" className="rounded-xl h-64" />
         </figure>
-        <div className="card-body items-center text-center">
+        <div className="card-body ">
+          <div className="flex items-center">
+            <img className="author-img w-10" src={author.image} alt="" />
+            <p className="flex justify-start ml-4">{author.name}</p>
+          </div>
           <h2 className="card-title">{title}</h2>
-          <p>{details.slice(0, 50)}</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="flex">
+            <p className="text-start flex items-center text-lg">
+              <IoIosTimer></IoIosTimer>
+              {courseTime}
+            </p>
+            <p className=" text-lg flex items-center ml-32">
+              <HiOutlineBookOpen className="text-end"></HiOutlineBookOpen>
+              {modules}
+            </p>
+          </div>
+          <div className="flex">
+            <p className="text-start text-lg font-semibold">${fees}</p>
+            <p className="ml-[190px] text-lg flex items-center">
+              <HiStar></HiStar>
+              {rating}
+            </p>
+          </div>
+          <div className="">
+            <button
+              onClick={() => {
+                handleDetails(id);
+              }}
+              className="btn btn-primary w-full"
+            >
+              Details
+            </button>
           </div>
         </div>
       </div>
